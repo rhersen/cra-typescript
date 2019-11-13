c6928fb0e52948112957d1ce91166de0f0c6d089
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-11-13 17:19:04 +0100

    Use lighter font for 'from'

--- src/Destination.tsx
+++ src/Destination.tsx
@@ -2,21 +2,22 @@ import React from "react";
 import TrainAnnouncement from "./TrainAnnouncement";
 import locations from "./locations";
 
-export default function Destination(props: { announcement: TrainAnnouncement }) {
+export default function Destination(props: {
+  announcement: TrainAnnouncement;
+}) {
   return (
-      <td className="destination">
+    <td className="destination">
       <span className="from">
         {props.announcement.FromLocation &&
-        props.announcement.FromLocation.map(location =>
+          props.announcement.FromLocation.map(location =>
             locations(location.LocationName)
-        ).join()}
+          ).join()}
         –
       </span>
-        {props.announcement.ToLocation &&
+      {props.announcement.ToLocation &&
         props.announcement.ToLocation.map(location =>
-            locations(location.LocationName)
+          locations(location.LocationName)
         ).join()}
-      </td>
-  )
-
+    </td>
+  );
 }
