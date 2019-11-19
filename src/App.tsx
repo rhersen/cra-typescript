003c89d38b3bd7b5517b81a2c06583004824757c
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-11-19 21:57:37 +0100

    Remove debug logging

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,10 @@ export default class App extends React.Component<{}, MyState> {
   };
 
   componentDidMount() {
-    console.log("componentDidMount");
     intervalId = setInterval(() => this.setState({ now: new Date() }), 990);
   }
 
   componentWillUnmount() {
-    console.log("componentWillUnmount");
     clearInterval(intervalId);
   }
 
