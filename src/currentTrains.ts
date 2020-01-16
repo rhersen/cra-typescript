65b9a6b347920cff17d06a3061a18846e3d2860b
Rudolf Hersén <rudolf.hersen@schibsted.se>
2020-01-16 22:00:53 +0100

    Port two files to typescript

---
+++ src/currentTrains.ts
@@ -0,0 +1,60 @@
+import _ from "lodash";
+import * as wgs from "./wgs";
+
+export default function currentTrains(announcement: any) {
+  const grouped = _.groupBy(announcement, "AdvertisedTrainIdent");
+  const object = _.filter(_.map(grouped, announcementsToObject), "actual");
+  const sorted = sortTrains(object, direction(announcement));
+  return _.reject(sorted, hasArrivedAtDestination);
+
+  function announcementsToObject(v: any) {
+    const actual = _.maxBy(
+      _.filter(v, "TimeAtLocation"),
+      a => a.TimeAtLocation + a.ActivityType
+    );
+    const next = _.minBy(
+      _.reject(v, "TimeAtLocation"),
+      a => a.AdvertisedTimeAtLocation + a.ActivityType
+    );
+
+    return { actual, next };
+  }
+
+  function direction(announcements: string | any[]) {
+    return (
+      announcements.length &&
+      /\d\d\d[13579]/.test(announcements[0].AdvertisedTrainIdent)
+    );
+  }
+
+  function hasArrivedAtDestination(train: any) {
+    return (
+      train.actual.ActivityType === "Ankomst" &&
+      _.map(train.actual.ToLocation, "LocationName").join() ===
+        train.actual.LocationSignature
+    );
+  }
+
+  function sortTrains(obj: { actual: any; next: any; }[], dir: number | boolean) {
+    return _.orderBy(
+      obj,
+      [
+        a => north(a.actual.LocationSignature),
+        "actual.ActivityType",
+        "actual.TimeAtLocation"
+      ],
+      ["desc", dir ? "asc" : "desc", dir ? "desc" : "asc"]
+    );
+  }
+
+  function north(location: string) {
+    if (location === "Gdv") return between("Ngd", "Nyh");
+    if (location === "Söc") return between("Söd", "Söu");
+    if (location === "Gn") return between("Mö", "Ssä");
+    return wgs.north(location);
+  }
+
+  function between(loc1: string, loc2: string) {
+    return 0.5 * wgs.north(loc1) + 0.5 * wgs.north(loc2);
+  }
+}
