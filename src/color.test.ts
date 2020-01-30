a8259de8af155c2b86685c0b55bd5ef91810144b
Rudolf Hersén <rudolf.hersen@schibsted.se>
2020-01-30 07:18:37 +0100

    remove the green/white/yellow interpolation

--- src/color.test.ts
+++ src/color.test.ts
@@ -6,13 +6,13 @@ describe("color", () => {
     expect(color(a("07:03:00"))).toEqual("rgb(0,256,0)"));
 
   it("30 seconds delay", () =>
-    expect(color(a("07:03:30"))).toEqual("rgb(128,256,128)"));
+    expect(color(a("07:03:30"))).toEqual("rgb(256,256,256)"));
 
   it("1 minute delay", () =>
     expect(color(a("07:04:00"))).toEqual("rgb(256,256,256)"));
 
   it("90 seconds delay", () =>
-    expect(color(a("07:04:30"))).toEqual("rgb(256,256,128)"));
+    expect(color(a("07:04:30"))).toEqual("rgb(256,256,256)"));
 
   it("2 minutes delay", () =>
     expect(color(a("07:05:00"))).toEqual("rgb(256,256,0)"));
@@ -24,13 +24,13 @@ describe("color", () => {
     expect(color(a("07:07:00"))).toEqual("rgb(256,128,0)"));
 
   it("6 minutes delay", () =>
-    expect(color(a("07:09:00"))).toEqual("rgb(256,64,0)"));
+    expect(color(a("07:09:00"))).toEqual("rgb(256,112,0)"));
 
   it("8 minutes delay", () =>
-    expect(color(a("07:11:00"))).toEqual("rgb(256,0,0)"));
+    expect(color(a("07:11:00"))).toEqual("rgb(256,96,0)"));
 
-  it("9 minutes delay", () =>
-    expect(color(a("07:12:00"))).toEqual("rgb(256,0,0)"));
+  it("20 minutes delay", () =>
+    expect(color(a("07:23:00"))).toEqual("rgb(256,0,0)"));
 });
 
 function a(t: string): TrainAnnouncement {
--- src/color.ts
+++ src/color.ts
@@ -7,21 +7,21 @@ export default function color(a: TrainAnnouncement): string {
   return `rgb(${r(delay)},${g(delay)},${b(delay)})`;
 
   function r(d: number) {
-    if (d < 60) return (d * 256) / 60;
+    if (d < 30) return 0;
     return 256;
   }
 
   function g(d: number) {
     if (d < 120) return 256;
     if (d < 240) return 128 + ((240 - d) * 128) / 120;
-    const span = 900;
+    const span = 960;
     if (d < 240 + span) return ((240 + span - d) * 128) / span;
     return 0;
   }
 
   function b(d: number) {
-    if (d < 60) return (d * 256) / 60;
-    if (d < 120) return ((120 - d) * 256) / 60;
+    if (d < 30) return 0;
+    if (d < 120) return 256;
     return 0;
   }
 
