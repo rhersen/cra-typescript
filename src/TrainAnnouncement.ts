2e11a200c04e3d7b90941c6e0c720156e533eebc
Rudolf Hersén <rudolf.hersen@schibsted.com>
2020-03-11 20:37:16 +0100

    remove unused TrainAnnouncement fields

--- src/TrainAnnouncement.ts
+++ src/TrainAnnouncement.ts
@@ -1,29 +1,12 @@
 export default interface TrainAnnouncement {
-  ActivityId: string;
   ActivityType: string;
-  Advertised: boolean;
   AdvertisedTimeAtLocation: string;
   AdvertisedTrainIdent: string;
-  Canceled: boolean;
-  Deviation: Description[];
-  EstimatedTimeAtLocation: string;
-  EstimatedTimeIsPreliminary: boolean;
-  FromLocation: Location[];
-  InformationOwner: string;
   LocationSignature: string;
-  ModifiedTime: string;
-  NewEquipment: number;
-  OtherInformation: Description[];
   ProductInformation: Description[];
-  ScheduledDepartureDateTime: string;
-  TechnicalTrainIdent: string;
   TimeAtLocation: string;
   TimeAtLocationWithSeconds: string;
   ToLocation: Location[];
-  TrackAtLocation: string;
-  TypeOfTraffic: Description;
-  ViaToLocation: Location[];
-  WebLink: string;
 }
 
 interface Location {
@@ -35,30 +18,6 @@ interface Location {
 export interface Description {
   Code: string;
   Description: string;
-}
-
-export function shortText(announcement: TrainAnnouncement): string {
-  const deviations: string[] = (announcement.Deviation || []).map(descriptions);
-
-  return deviations
-    ? deviations
-        .map(s => {
-          if (/Kort/.test(s)) return "K";
-          if (s.length) return "*";
-          return s;
-        })
-        .join()
-    : "";
-}
-
-export function deviationText(announcement: TrainAnnouncement): string[] {
-  return (announcement.Deviation || [])
-    .map(descriptions)
-    .filter(s => !/Kort/.test(s));
-}
-
-function descriptions(deviation: Description): string {
-  return deviation.Description;
 }
 
 export function key(announcement: TrainAnnouncement) {
